624e215828e998c42305a6ab07d5a055ad4a207c
ouss223 <oussama.ghazouani@insat.ucar.tn>
2025-01-17 22:34:52 +0100

    progress in navbar

--- my-next-app/app/(root)/page.tsx
+++ my-next-app/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-blue-400">Home</h1>
+      <h1 className="text-3xl text-blue-600 ">Home</h1>
     </div>
   );
 }
--- my-next-app/app/components/Navbar.tsx
+++ my-next-app/app/components/Navbar.tsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-const Navbar = () => {
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div className='px-10 py-10 bg-white shadow-md font-work-sans'>
-        <nav className='flex justify-between items-center'>
-            <Link href="/">
-            <Image src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" alt="animal" width={100} height={100} unoptimized />
-            
-            </Link>
-            </nav></div>
-  )
-}
+    <div className="px-10 py-10 bg-white shadow-md font-work-sans">
+      <nav className="flex justify-between items-center ">
+        <Link href="/">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg"
+            alt="animal"
+            width={100}
+            height={100}
+            unoptimized
+          />
+        </Link>
+        <div className="flex items-center gap-10 text-black">
+          {session && session?.user ? (
+            <div>
+              <Link href="/profile">
+                <a className="">Profile</a>
+              </Link>
+              <button onClick={() => signOut()} className="">
+                Sign Out
+              </button>
+            </div>
+          ) : (
+            <button
+              className=""
+              onClick={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
