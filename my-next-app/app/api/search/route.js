cad24ab1a956a0577b836c89c9de7ff5f69de305
ouss223 <oussama.ghazouani@insat.ucar.tn>
2025-02-01 22:18:30 +0100

    modified the search api for better usage

--- my-next-app/app/api/search/route.js
+++ my-next-app/app/api/search/route.js
@@ -4,29 +4,27 @@ import { authenticateRequest } from "../authenticateRequest";
 
 export async function POST(request) {
   try {
-    const { searchQuery} =
-      await request.json();
+    const { searchQuery, page } = await request.json();
     const userId = authenticateRequest(request);
     const searchQueryQuery = `
             SELECT * FROM cards 
             WHERE title LIKE '%?%' OR description LIKE '%?%'
+            LIMIT 10 OFFSET ?
             
         `;
+    const offset = page * 10;
+
     const searchResult = await new Promise((resolve, reject) => {
-        db.query(
-            searchQueryQuery,
-            [searchQuery, searchQuery],
-            (err, result) => (err ? reject(err) : resolve(result))
-        );
-        }
-    );
+      db.query(
+        searchQueryQuery,
+        [searchQuery, searchQuery, offset],
+        (err, result) => (err ? reject(err) : resolve(result))
+      );
+    });
     return NextResponse.json({
       message: "Cards retrieved successfully",
-      searchResult
+      searchResult,
     });
-
-
-   
   } catch (error) {
     console.error("Error in POST request:", error);
     return NextResponse.json(
